ced43a9c5c7a03257d3a18fba7f70eed1d3af392
Hans <hansyanghk@gmail.com>
2026-04-22 16:27:05 -0400

    Athlete info function

--- src/olympoly/olympics_betting/athlete_info.py
+++ src/olympoly/olympics_betting/athlete_info.py
@@ -1,3 +1,13 @@
 # Hans and Lauren
 # script to standardize string input with pandas function
 # return athlete data
+
+from olympoly.load_data import load_olympic_data
+
+def get_athlete_info(athlete_name: str):
+    df = load_olympic_data()
+    athlete_info = df[df["Name"].str.contains(athlete_name, case=False)]
+    print(athlete_info)
+    return None
+
+print(get_athlete_info("Michael Fred Phelps, II"))
